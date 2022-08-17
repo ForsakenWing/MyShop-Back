0d85f5d7fbcb435aa530286b2593cd143fd200e8
ForsakenWing <Fors2288@gmail.com>
2022-08-17 16:06:02 +0200

    Checking readiness for merging into master

---
+++ tests/Common/base_v1.py
@@ -0,0 +1,4 @@
+class BaseV1:
+
+    token = None
+    headers = None
--- tests/test_v1/test_v1.py
+++ tests/test_v1/test_v1.py
@@ -1,5 +1,6 @@
 from requests import request
-from src.core import Token
+from src.core import Token, Data, User
+from tests.Common.base_v1 import BaseV1
 import pytest
 
 
@@ -10,6 +11,20 @@ def url_fixture() -> str:
 
 
 class TestV1EndToEnd:
+
+    def test_create_new_user(self, url):
+        response = request(
+            method="post",
+            url=f"{url}/api/v1/user/new",
+            json=dict(
+                username="test_user_to_get_token",
+                email="user@example.com",
+                password="test_password",
+            )
+        )
+        BaseV1.token = response.json()['data']['token']['access_token']
+        assert response.status_code == 201
+        assert Data.validate(response.json()['data'])
 
     def test_login_for_access_token(self, url):
         response = request(
@@ -23,4 +38,20 @@ class TestV1EndToEnd:
         assert response.status_code == 200
         assert Token.validate(response.json())
 
-    # def test_create_new_user(self):
+    def test_get_user_data(self, url):
+        response = request(
+            method="get",
+            url=f"{url}/api/v1/user/me",
+            headers={"Authorization": f'Bearer {BaseV1.token}'}
+        )
+        assert response.status_code == 200
+        assert User.validate(response.json())
+
+    def test_delete_user(self, url):
+        response = request(
+            method="delete",
+            url=f"{url}/api/v1/user/me/delete",
+            headers={"Authorization": f'Bearer {BaseV1.token}'}
+        )
+        assert response.status_code == 200
+        assert response.json() == {"Status": "Successful removing"}
