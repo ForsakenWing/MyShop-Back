322f24ff0ec85b743ff60f648ba0a324a1a4a2b7
ForsakenWing <Fors2288@gmail.com>
2022-08-21 17:06:07 +0200

    Implemented behavior tests

---
+++ features/steps/create_user.py
@@ -0,0 +1,39 @@
+from behave import given, when, then
+from faker import Faker
+from requests import request
+
+
+fake = Faker()
+url = "http://0.0.0.0:8088"
+
+
+@given('User with valid data')
+def get_valid_user_data(context):
+    username = fake.first_name()
+    password = fake.password()
+    email = fake.email()
+    return dict(
+        username=username,
+        password=password,
+        email=email
+    )
+
+
+@when('Send request to api/v1/user/new endpoint')
+def create_user(context):
+    user_data = get_valid_user_data(context)
+    response = request(
+        method="post",
+        url=f"{url}/api/v1/user/new",
+        json=user_data
+    )
+    del user_data['password']
+    return response, user_data
+
+
+@then('User successfully created')
+def assert_that_user_successfully_created(context):
+    response, user_data = create_user(context)
+    for key in user_data:
+        assert response.json()['data']['user'][key] == user_data[key]
+    assert response.status_code == 201
---
+++ features/user/delete_user.feature
@@ -0,0 +1,5 @@
+Feature: Delete user
+  Deletion user through delete endpoint
+
+  Scenario: Positive scenario deletion user test
+    # Enter steps here
